5da2452767a86d7c5cea3b40a1711e4b0c12f44d
bruno <akmacicbruno@gmail.com>
2021-06-25 22:15:54 +0200

    Animacija za menu

--- js/animation.js
+++ js/animation.js
@@ -1,24 +1,33 @@
 function Animation() {
     var x = document.getElementById("links");
     var y = document.getElementById("login");
+    var p = document.getElementById("pic_down");
     var z = document.getElementById("content");
     var h = document.getElementById("hamburger");
     var c = document.getElementById("close");
-    x.style.display = "block";
-    y.style.display = "block";
-    z.style.top = "250px";
+    x.style.display = "inline-grid";
+    x.style.position = "absolute";
+    x.style.top = "60px";
+    y.style.display = "inline-grid";
+    y.style.position = "absolute";
+    y.style.top = "600px";
+    y.style.left = "31%";
+    p.style.bottom = "-22px";
+    z.style.display = "none";
     c.style.display = "block";
     h.style.display = "none";
   };
 function AnimationClose() {
     var x = document.getElementById("links");
     var y = document.getElementById("login");
+    var p = document.getElementById("pic_down");
     var z = document.getElementById("content");
     var h = document.getElementById("hamburger");
     var c = document.getElementById("close");
     x.style.display = "none";
     y.style.display = "none";
-    z.style.top = "100px";
+    p.style.bottom = "-230px";
+    z.style.display = "block";
     c.style.display = "none";
     h.style.display = "block";
   }
